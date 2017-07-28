b7fdc8ee0100d10fa0261f69197ca90a448193af
Kelsey Mason <kmason@usgs.gov>
2017-07-28 13:43:17 -0600

    Got code to work exiftool geotag and move files

--- ExifTool_Geotag.py
+++ ExifTool_Geotag.py
@@ -13,18 +13,26 @@ geotagpath = path + '\JPEGs\Geotagged'
 date = time.strftime("%B") + time.strftime("%d%Y")
 #Place GPX files in GPX folder
 #Place JPGs in JPGs folder
-exiftoolcmd = 'exiftool -config .ExifTool_config -geotag=28.BIN.gpx -geotag=29.BIN.gpx -geotag=30.BIN.gpx "-geotime<${DateTimeOriginal}+00:00"' + ' ' + jpgspath
-#run exiftool
-os.system(exiftoolcmd)
+geotagcmd = 'exiftool -config .ExifTool_config -geotag "GPXs\*.BIN.gpx" "-geotime<${DateTimeOriginal}+00:00" JPEGs'
+#run exiftool to geotag
+os.system(geotagcmd)
+#remove orientation tag
+remorient = 'exiftool -Orientation= JPEGs\*.JPG'
+os.system(remorient)
 #move originals to new folder called "Originals"
 os.chdir(originalspath)
-os.system('mkdir ' + date)
+name = date
+num = 1
+while os.path.exists(originalspath + '/' + name):
+    name = name.split('_')
+    name = str(name[0]) + '_' + str(num)
+    num = num + 1
+os.system('mkdir ' + name)
 os.chdir(donepath)
-os.system('mkdir ' + date)
+os.system('mkdir ' + name)
 os.chdir(geotagpath)
-os.system('mkdir ' + date)
-
-os.chdir(originalspath + '/' + date)
+os.system('mkdir ' + name)
+os.chdir(originalspath + '/' + name)
 movejpgscmd = 'copy ' + jpgspath + '\*.jpg_original'
 os.system(movejpgscmd)
 #rename originals to just .jpg
@@ -33,14 +41,14 @@ os.system('rename *.jpg_original *.jpg')
 os.chdir(jpgspath)
 os.system('del *.jpg_original')
 #move geotagged to new folder called "Geotagged"
-os.chdir(geotagpath + '/' + date)
+os.chdir(geotagpath + '/' + name)
 movejpgscmd2 = 'copy ' + jpgspath + '\*.jpg'
 os.system(movejpgscmd2)
 #delete geotagged from folder
 os.chdir(jpgspath)
 os.system('del *.jpg')
 #move GPX files to "Done" folder
-os.chdir(donepath + '/' + date)
+os.chdir(donepath + '/' + name)
 movegpxcmd = 'copy ' + gpxpath + "\*.BIN.gpx"
 os.system(movegpxcmd)
 #delete GPX files
